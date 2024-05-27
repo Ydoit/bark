6d9de259806d3855d65b087e6a55937d6a4072bb
ydoit <yandoit@163.com>
2024-05-27 08:00:07 +0000

    update

--- output/random/zh/mkdir.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-for i in range(1, 11):
-    if not os.path.exists(f"speaker{i}"):
-        os.mkdir(f"speaker{i}")
-# for i in range(1, 11):
-#     if not os.path.exists(f"zh/speaker{i}"):
-#         os.mkdir(f"zh/speaker{i}")
